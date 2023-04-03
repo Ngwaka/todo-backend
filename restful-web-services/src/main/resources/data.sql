751167bd166a9eab6d99b84ecea9af1287dd0111
Claude Ekango Ngwaka <claude.ngwaka@alten.it>
2023-04-03 12:39:44 +0200

    105-step 94 Connecting POST , PUT , and DELETE REST APIs to jpa repository

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,14 +1,14 @@
  insert into TODO(id, username, description, target_date, is_done)
-values(1001, 'in28mintes', 'Learn Microservices', CURRENT_TIMESTAMP() , false ) ;
+values(1001, 'in28minutes', 'Learn Microservices', CURRENT_TIMESTAMP() , false ) ;
 
  insert into TODO(id, username, description, target_date, is_done)
-values(1002, 'in28mintes', 'Learn JPA', CURRENT_TIMESTAMP() , false ) ;
+values(1002, 'in28minutes', 'Learn JPA', CURRENT_TIMESTAMP() , false ) ;
 
  insert into TODO(id, username, description, target_date, is_done)
-values(1003, 'in28mintes', 'Learn Docker', CURRENT_TIMESTAMP() , false ) ;
+values(1003, 'in28minutes', 'Learn Docker', CURRENT_TIMESTAMP() , false ) ;
 
  insert into TODO(id, username, description, target_date, is_done)
-values(1004, 'in28mintes', 'Learn Kubernates', CURRENT_TIMESTAMP() , false ) ;
+values(1004, 'in28minutes', 'Learn Kubernates', CURRENT_TIMESTAMP() , false ) ;
 
  insert into TODO(id, username, description, target_date, is_done)
-values(1005, 'in28mintes', 'Visit Dschang', CURRENT_TIMESTAMP() , false ) ;
+values(1005, 'in28minutes', 'Visit Dschang', CURRENT_TIMESTAMP() , false ) ;
